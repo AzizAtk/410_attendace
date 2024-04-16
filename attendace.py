7d0c5da566d23fd0c0a1b439c2f366d88121f845
Abdulaziz Alateeqi <113974863+AzizAtk@users.noreply.github.com>
2024-04-16 14:20:57 -0700

    Update attendace.py

--- attendace.py
+++ attendace.py
@@ -53,12 +53,16 @@ while True:
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
     rgb_small_frame = small_frame[:, :, ::-1]
     now = datetime.now()
+    frame_skip = 5  # Only process every 5th frame
+    count = 0
+
     if s:
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_landmarks_list = face_recognition.face_landmarks(rgb_small_frame)
         face_names = []
 
-        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        for face_location, face_encoding, face_landmarks in zip(face_locations, face_encodings, face_landmarks_list):
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = ""
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
@@ -71,15 +75,19 @@ while True:
             face_names.append(name)
             if name:
                 # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
+                top, right, bottom, left = [x * 4 for x in face_location]
 
                 # Draw a box around the face
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-                # Draw label with a name below the face
+                # Highlighting the facial features
+                # Highlighting the facial features
+                #for facial_feature in ['left_eye', 'right_eye', 'top_lip', 'bottom_lip', 'nose_bridge', 'nose_tip']:
+                    #points = [(x * 4, y * 4) for (x, y) in face_landmarks[facial_feature]]
+                    #cv2.polylines(frame, [np.array(points, dtype=np.int32)], isClosed=False, color=(255, 255, 0), thickness=1)
+
+
+                # Draw label with a name and accuracy below the face
                 #cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
                 font = cv2.FONT_HERSHEY_DUPLEX
                 text = f"{name} ({accuracy:.2f}%)"
